b69836ea3ce9d38e0a405c5fb12091f841d15a67
YarnSeemannsgarn <janschlenker1990@gmail.com>
2016-01-25 18:57:33 +0100

    Finish code

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 Requirements
 ============
 
+The programme was tested under Ubuntu
+
 - GDAL
 
-    sudo apt-get install gdal-bin libgdal-dev
+    $ sudo apt-get install gdal-bin libgdal-dev
 
 - Python GDAL support
 
-    sudo apt-get install python-gdal
+    $ sudo apt-get install python-gdal
+
+- Python matplotlib and python-tk for plotting
+
+    $ sudo apt-get install python-matplotlib
+    $ sudo apt-get install python-tk
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import json
-from gemeinden_tirol import gemeinden_tirol
+import matplotlib.pyplot as plt
+from communities_tirol import communitiesTirol
 from osgeo import ogr
 from LatLongUTMconversion import UTMtoLL
+import dateutil.parser
+import datetime
 
 ELLIPSOID_ID = 23
 
@@ -36,7 +38,7 @@ Reads specified shapefile and returns polygons.
 Polygon format: [(x1, y1), (x2, y2), ...] 
 """
 def readShapfile(filePath):
-    driver = ogr.GetDriverByName('ESRI Shapefile')
+    driver = ogr.GetDriverByName("ESRI Shapefile")
     datasource = driver.Open(filePath, 0)
     layer = datasource.GetLayer()
 
@@ -60,22 +62,72 @@ def readShapfile(filePath):
     return polygons
 
 """
+Read tweet data and change communityTweets and datesTweets
+communityTweets --> amount of tweets per community
+datesTweets --> amount of tweets per date
+"""
+def readTweets(data, polygons, communitiesTweets, datesTweets):
+    for tweet in data["features"]:
+        coordinates = tweet["geometry"]["coordinates"]
+        for i, polygon in enumerate(polygons):
+            if coordinates[0] > 10 and coordinates[0] < 14 and coordinates[1] > 46 and coordinates[1] < 48 and pointInPolygon(coordinates[1], coordinates[0], polygon):
+                community = communitiesTirol[i]
+                if community in communitiesTweets:
+                    communitiesTweets[community] = communitiesTweets[community] + 1
+                else:
+                    communitiesTweets[community] = 1
+                
+                dateTimeISO = tweet["properties"]["isots"]
+                dateTimeParsed = dateutil.parser.parse(dateTimeISO)
+                datesTweets[dateTimeParsed.day-1] = datesTweets[dateTimeParsed.day-1] + 1
+                print datesTweets
+                break
+
+"""
+Saves the tweets per community to a file
+"""
+def saveCommunityTweets(communitiesTweets):
+    f = open("community_tweets.csv", 'w')
+    for communityTweets in communitiesTweets:
+        f.write("{0}; {1}\n".format(communityTweets, communitiesTweets[communityTweets]))
+    f.close()
+
+
+"""
+Plots the tweets per date
+"""
+def plotDatesTweets(datesTweets):
+    # Plot datesTweets
+    size = plt.rcParams["figure.figsize"]
+    plt.rcParams["figure.figsize"] = [size[0]*3, size[1]]
+    xAxis = []
+    for i in xrange(27):
+        xAxis.append(datetime.date(2014, 04, i+1))
+    plt.plot(xAxis, datesTweets)
+    plt.savefig('dates_tweets.png')
+
+
+"""
 MAIN METHOD - PROGRAMM STARTING POINT
 """
 def main():
     # Read shapefile
-    polygons = readShapfile("gemeindekarte/Export_Output_2.shp")
+    polygons = readShapfile("communities_tirol_map/Export_Output_2.shp")
 
     # Read geojson
     with open("boundingbox1.geojson") as f:
         data = json.load(f)
 
     # Read tweets and check polygons
-    for tweet in data["features"]:
-        coordinates = tweet["geometry"]["coordinates"]
-        for i, polygon in enumerate(polygons):
-            if coordinates[0] > 10 and coordinates[0] < 14 and coordinates[1] > 46 and coordinates[1] < 48 and pointInPolygon(coordinates[1], coordinates[0], polygon):
-                print "{0}; {1}".format(gemeinden_tirol[i], tweet["properties"]["isots"])
+    communitiesTweets = {}
+    datesTweets = [0] * 27
+    readTweets(data, polygons, communitiesTweets, datesTweets)
+    
+    # Save communityTweets
+    saveCommunityTweets(communitiesTweets)
+    
+    # Plot datesTweets
+    plotDatesTweets(datesTweets)
 
 if __name__ == '__main__':
     main()
